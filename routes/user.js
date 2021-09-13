fde26b5622be4bec60bd3a0881d6ff6b107fea0c
Iryna <32779093+irin2811@users.noreply.github.com>
2021-09-13 23:38:05 +0300

    Delete user.js

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const users = require('../controllers/user');
-
-module.exports = (app) => {
-    app.get('/user_list', users.getAll);
-    app.post('/user', users.create);
-    app.put('/user/:id', users.update);
-    app.delete('/user/:id', users.remove);
-};
