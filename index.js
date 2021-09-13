b760398317c9d5eb60ee76f7afd23d0c826e1d6f
Iryna <32779093+irin2811@users.noreply.github.com>
2021-09-13 23:32:43 +0300

    Add files via upload
    add version folders for future changes.
    add general index-files to route folder and model folder

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/app'); // project global constans
-const Pet = require('./models/pet_model');
-const Farm = require('./models/farm_model');
-const User = require('./models/user_model');
+const Model = require('./models');
 
 const app = express();
 require('./config/express')(app); // web-server settings
-require('./routes/pet')(app); // request for Pet model
-require('./routes/farm')(app); // request for Farm model
-require('./routes/user')(app); // request for User model
+require('./routes/index')(app); // requests for Pet, Farm and User models
 
 mongoose.connect(config.mongoUrl, function(err) {
     if(err) throw err;
