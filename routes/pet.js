09db51ecf8e4cc95852e36d55f3c6d842b7c5f65
Iryna <32779093+irin2811@users.noreply.github.com>
2021-09-13 23:37:59 +0300

    Delete pet.js

--- routes/pet.js
+++
@@ -1,8 +0,0 @@
-const pets = require('../controllers/pet');
-
-module.exports = (app) => {
-    app.get('/pet_list', pets.getAll);
-    app.post('/pet', pets.create);
-    app.put('/pet/:id', pets.update);
-    app.delete('/pet/:id', pets.remove);
-};
